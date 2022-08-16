e152db2f44759660c8f012f62bc7b644b2e55558
Gabriel4g <gabrielprogramador11@gmail.com>
2022-08-15 23:45:22 -0500

    first

---
+++ js/main.js
@@ -0,0 +1,64 @@
+function playAudio(audio) {
+    audio.currentTime = 0.1
+    audio.play()
+}
+
+function Cavalo() {
+    playAudio(document.querySelector("#cavalo"));
+}
+
+function DançaGatinho() {
+    playAudio(document.querySelector("#dança"));
+}
+
+function EleGosta() {
+    playAudio(document.querySelector("#ele-gosta"));
+}
+
+function Tome() {
+    playAudio(document.querySelector("#tome"));
+}
+
+function Pare() {
+    playAudio(document.querySelector("#pare"));
+}
+
+function Demais() {
+    playAudio(document.querySelector("#demais"));
+}
+
+function Ui() {
+    playAudio(document.querySelector("#ui"));
+}
+
+function Rapaz() {
+    playAudio(document.querySelector("#rapaz"));
+}
+
+function AiAi() {
+    playAudio(document.querySelector("#aiai"));
+}
+
+function Xii() {
+    playAudio(document.querySelector("#xii"));
+}
+
+function Brincadeira() {
+    playAudio(document.querySelector("#brincadeira"));
+}
+
+function Tapa() {
+    playAudio(document.querySelector("#tapa"));
+}
+
+function Chega() {
+    playAudio(document.querySelector("#chega"));
+}
+
+function Brasil() {
+    playAudio(document.querySelector("#brasil"));
+}
+
+function Uepa() {
+    playAudio(document.querySelector("#uepa"));
+}
